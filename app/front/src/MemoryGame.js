6457a7c3e8dd10c8fbc83efabcfa86d8fd0f3a8d
renomarx <renomarx@gmail.com>
2020-02-22 16:54:02 +0100

    front: more comments on code

--- app/front/src/MemoryGame.js
+++ app/front/src/MemoryGame.js
@@ -6,19 +6,25 @@ import MemoryTimer from './MemoryTimer.js';
 import { getRandomInt } from './utils.js';
 import { APIUrl } from './config.js';
 
+// Composant gérant la logique du jeu memory
 class MemoryGame extends React.Component {
   constructor(props) {
     super(props);
+    // On commence par mélanger les cartes
     let cards = this.shuffleCards()
+
+    // On initialise le state
     this.state = {
-      cards: cards,
-      firstCardTurned: null,
-      secondCardTurned: null,
-      cardsWon: {},
-      time: 0,
-      scoresLoaded: false,
-      scores: []
+      cards: cards, // cartes mélangées
+      firstCardTurned: null, // Première carte tournée sur un tour
+      secondCardTurned: null, // Seconde carte tournée sur un tour
+      cardsWon: {}, // Cartes gagnées
+      time: 0, // Temps écoulé
+      scoresLoaded: false, // Si les scores sont chargés depuis le serveur
+      scores: [] // Tableau des 3 meilleurs scores
     };
+
+    // Permet d'accéder aux propriétés et méthodes de la classe depuis ces méthodes
     this.ClickCard = this.ClickCard.bind(this);
     this.shuffleCards = this.shuffleCards.bind(this);
     this.checkWin = this.checkWin.bind(this);
@@ -27,14 +33,16 @@ class MemoryGame extends React.Component {
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
 
+    // Variable contenant l'id du timer généré par setInterval
     this.timer = null;
-    this.time = 0;
   }
 
+  // Au montage du composant, on va chercher les meilleurs scores sur le serveur
   componentDidMount() {
     this.fetchBestScores()
   }
 
+  // Fonction allant chercher les scores sur le serveur
   fetchBestScores() {
     fetch(APIUrl + "/scores?limit=3")
       .then(res => res.json())
@@ -46,28 +54,46 @@ class MemoryGame extends React.Component {
           });
         },
         (error) => {
+          // Si il y a une erreur, on loggue mais on ne coupe pas le script,
+          // car on peut jouer quand même
           console.error(error)
         }
       )
   }
 
+  // Fonction mélangeant les cartes
   shuffleCards() {
     let cardsTotal = this.props.cardsNumber * 2
     if (cardsTotal > 36) {
       console.error("Le jeu ne peut tourner qu'avec 36 cartes (18 différentes) maximum")
     }
     let cards = [];
-    let availableValues = [...Array(cardsTotal).keys()]; // [0,1,2,3,...,34,35]
+    // On commence par générer le tableau de toutes les valeurs possibles, en double:
+    // Petit trick utilisé:
+    // si on a 18 cartes différentes demandées, on génère des valeurs jusqu'à 18 * 2 = 36
+    let availableValues = [...Array(cardsTotal).keys()]; // Ex: [0,1,2,3,...,34,35]
+    // Nous allons générer autant de cartes que nécessaire
     for (let i = 0; i < cardsTotal; i++) {
+      // Une card est un objet contenant 3 propriétés:
+      // - pos : sa position sur le plateau
+      // - turned : son état (retourné ou non)
+      // - val : sa valeur (fruit correspondant)
       let card = {pos: i, turned: false};
+      // On choisit une valeur random parmi toutes celles possibles
       let r = getRandomInt(availableValues.length);
+      // Suite du petit trick:
+      // La valeur de la carte est la valeur random % 2
+      // Ainsi 17 = 17 mais 35 = 17 aussi
+      // Permet donc d'avoir sur le plateau exactement 2 cartes de même valeur
       card.val = availableValues[r] % this.props.cardsNumber;
       cards.push(card);
+      // Enfin, on supprime la valeur random utilisée des valeurs possibles
       availableValues.splice(r, 1);
     }
     return cards
   }
 
+  // Fonction déclenchée lorsqu'on clique sur une carte
   ClickCard(i) {
     // Toujours cloner le state pour changer d'état
     // Pour les objets, on utilise newState = { ...state } pour cloner un objet
@@ -85,6 +111,7 @@ class MemoryGame extends React.Component {
       return
     }
 
+    // Si le timer n'est pas lancé, on le démarre (1er click)
     if (this.timer === null) {
       this.startTimer()
     }
@@ -121,6 +148,7 @@ class MemoryGame extends React.Component {
     }
   }
 
+  // Méthode détectant si on a a gagné et si oui lançant les actions nécessaires
   checkWin() {
     if (Object.keys(this.state.cardsWon).length === this.props.cardsNumber) {
       this.stopTimer()
@@ -135,7 +163,9 @@ class MemoryGame extends React.Component {
     }
   }
 
+  // Méthode détectant si on a a perdu et si oui lançant les actions nécessaires
   checkLoose() {
+    // Si le timer n'a pas déjà été stoppé, et que le temps est supérieur au temps limite
     if (this.timer != null && this.state.time >= this.props.maxTime) {
       this.stopTimer()
       setTimeout(() => {
@@ -145,9 +175,10 @@ class MemoryGame extends React.Component {
     }
   }
 
+  // Méthode réinitialisant le plateau de cartes
   reset() {
+    this.stopTimer() // Au cas où
     let cards = this.shuffleCards()
-    this.stopTimer()
     this.setState({
       cards: cards,
       firstCardTurned: null,
@@ -157,14 +188,19 @@ class MemoryGame extends React.Component {
     });
   }
 
+  // Lance le timer
   startTimer() {
+    // setInterval permet d'appeler une fonction à intervalle régulier
+    // ici 200 millisecondes
     this.timer = setInterval(() => {
       this.setState({time: this.state.time + 200})
       this.checkLoose()
     }, 200);
   }
 
+  // Arrête le timer
   stopTimer() {
+    // Si il n'a pas déjà été arrêté et si il est lancé
     if (this.timer !== null) {
       clearInterval(this.timer)
       this.timer = null
@@ -197,6 +233,8 @@ class MemoryGame extends React.Component {
   }
 
   render() {
+    // Le pourcentage de la barre de progression est calculé en fonction du temps passé et du temps max
+    // On s'assure aussi que ce pourcentage ne dépasse pas 100 grâce à la fonction Math.min
     return (
       <div className="Game">
         {this.state.time === 0 ? <ScoresBoard scores={this.state.scores} /> : null}
--- app/front/src/utils.js
+++ app/front/src/utils.js
@@ -1,3 +1,4 @@
+// Fonction utilitaires
 
 export function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
