57d70155aa692fa1a6030e0a1d867d248dff4b3b
renomarx <renomarx@gmail.com>
2020-02-23 19:33:17 +0100

    back: refactoring + functional and unit tests

--- app/back/app.js
+++ app/back/app.js
@@ -1,57 +1,4 @@
 const models  = require('./models');
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-let port = 3333;
-
-// Middleware permettant de décoder le json du body d'une requête,
-// avant de le passer au handler de route
-app.use(express.json());
-
-// Middleware permettant de donner les bons headers CORS à chaque réponse
-app.use(cors())
-
-// Route pour récupérer tous les scores
-app.get('/scores', (req, res) => {
-  let options = {
-    order: [
-      ['duration', 'ASC'], // Pour avoir les meilleurs en premier
-    ],
-    limit: 10, // Par défaut, on limite à 10 résultats
-  }
-  // On permet de surcharger la limite par un paramètre GET
-  if (req.query.limit) {
-    options.limit = req.query.limit;
-  }
-  // Sequelize gènèrera le SQL permettant d'aller chercher les scores, selon nos options
-  models.Score.findAll(options).then(function(scores) {
-    res.status(200).json(scores)
-  });
-});
-
-// Route pour insérer un nouveau score
-app.post('/scores', (req, res) => {
-  let score = req.body;
-  // On contrôle si on a bien un score différent de 0ms (faut pas déconner)
-  if (score.duration === undefined || score.duration === 0) {
-    return res.status(400).json({ error: "Score duration cannot be 0."})
-  }
-  // L'ORM sequelize s'occupe de transformer cet appel create en requête SQL,
-  // en fonction du model déclaré dans models/score.js
-  models.Score.create(score).then((score) => {
-    // Si tout s'est bien passé, on renvoie un statut 200 OK,
-    // avec l'objet score nouvellement créé, encodé en json, en contenu de la réponse
-    res.status(200).json(score);
-  }).catch(err => {
-    // Si une erreur quelconque arrive, on loggue dans la console,
-    // et on renvoie un code d'erreur 500, pour que le client soit informé
-    // (sinon il attendrait indéfiniment une réponse en cas d'erreur)
-    console.error(err);
-    res.status(500).json({ error: "An error occured during save."});
-  })
-
-});
 
 // Permet à l'ORM sequelize de créer automatiquement les tables en BDD, si elles n'existent pas déjà
 // Sequelize se base sur les objets déclarés dans models/*.js
@@ -59,5 +6,5 @@ models.sequelize.sync().then(() => {
   // Si la synchronisation de la BDD s'est bien passé, on peut lancer le serveur API
   console.log("Database models were synchronized successfully.");
 
-  app.listen(port, () => console.log(`Memory server listening on port ${port}!`));
+  require("./server");
 })
---
+++ app/back/server.test.js
@@ -0,0 +1,127 @@
+var request = require('supertest');
+
+describe('Testing app workflow', function () {
+  let server;
+
+  beforeEach(async function () {
+    const models  = require('./models');
+    await models.sequelize.sync()
+    server = require('./server');
+  });
+  afterEach(function () {
+    server.close();
+  });
+
+
+  it('responds to /scores', function testGetScores(done) {
+  request(server)
+    .get('/scores')
+    .expect(200, [], done);
+  });
+
+  it('404 everything else', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
+  });
+
+  it('should create a score', function(done) {
+    request(server)
+      .post('/scores')
+      .send({
+        duration: 42
+      })
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body.createdAt = '2020-02-22T19:25:00Z';
+        res.body.updatedAt = '2020-02-22T19:25:00Z';
+      })
+      .expect(200, {
+        id: 1,
+        duration: 42,
+        createdAt: '2020-02-22T19:25:00Z',
+        updatedAt: '2020-02-22T19:25:00Z'
+      }, done);
+  });
+
+  it('should now have one score', function(done) {
+    request(server)
+      .get('/scores')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body[0].createdAt = '2020-02-22T19:25:00Z';
+        res.body[0].updatedAt = '2020-02-22T19:25:00Z';
+      })
+      .expect(200, [{
+        id: 1,
+        duration: 42,
+        createdAt: '2020-02-22T19:25:00Z',
+        updatedAt: '2020-02-22T19:25:00Z'
+      }], done);
+  });
+
+  it('should create a second better score', function(done) {
+    request(server)
+      .post('/scores')
+      .send({
+        duration: 21
+      })
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body.createdAt = '2020-02-22T19:25:00Z';
+        res.body.updatedAt = '2020-02-22T19:25:00Z';
+      })
+      .expect(200, {
+        id: 2,
+        duration: 21,
+        createdAt: '2020-02-22T19:25:00Z',
+        updatedAt: '2020-02-22T19:25:00Z'
+      }, done);
+  });
+
+  it('should now have two scores, ordered by best first', function(done) {
+    request(server)
+      .get('/scores')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body[0].createdAt = '2020-02-22T19:25:00Z';
+        res.body[0].updatedAt = '2020-02-22T19:25:00Z';
+        res.body[1].createdAt = '2020-02-22T19:25:00Z';
+        res.body[1].updatedAt = '2020-02-22T19:25:00Z';
+      })
+      .expect(200, [
+        {
+          id: 2,
+          duration: 21,
+          createdAt: '2020-02-22T19:25:00Z',
+          updatedAt: '2020-02-22T19:25:00Z'
+        },
+        {
+          id: 1,
+          duration: 42,
+          createdAt: '2020-02-22T19:25:00Z',
+          updatedAt: '2020-02-22T19:25:00Z'
+        },
+      ], done);
+  });
+
+  it('should limit returned scores', function(done) {
+    request(server)
+      .get('/scores?limit=1')
+      .set('Accept', 'application/json')
+      .expect(function(res) {
+        res.body[0].createdAt = '2020-02-22T19:25:00Z';
+        res.body[0].updatedAt = '2020-02-22T19:25:00Z';
+      })
+      .expect(200, [
+        {
+          id: 2,
+          duration: 21,
+          createdAt: '2020-02-22T19:25:00Z',
+          updatedAt: '2020-02-22T19:25:00Z'
+        },
+      ], done);
+  });
+
+
+});
